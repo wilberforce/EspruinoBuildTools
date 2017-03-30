384105fb645dde8ea29b002b4ba41cbff4440d26
wilberforce <github@wilberforce.co.nz>
2017-03-31 07:55:05 +1300

    prepare for esp-idf v2.0-rc2

--- esp32/build/build.sh
+++ esp32/build/build.sh
@@ -1,6 +1,17 @@
-if [ ! -d "Espruino" ]; then
-git clone https://github.com/espruino/Espruino.git
-fi
-source ./Espruino/scripts/provision-framework.sh ESP32
-#cd Espruino
-#make clean && make
+pushd Espruino/
+source scripts/provision.sh ESP32
+popd
+export ESP_IDF_PATH=`pwd`/esp-idf
+cd esp-idf
+git checkout -f master
+#git checkout -f v2.0-rc2
+git checkout -f tags/v2.0-rc2
+git pull
+git submodule update --init
+cd ../app
+make clean && make -j 8
+make app.tgz
+cd ../Espruino/
+tar xfz ../../deploy/esp-idf.tgz
+tar xfz ../../deploy/app.tgz
+make clean && BOARD=ESP32 USE_FLASH_FILESYSTEM=1 make
